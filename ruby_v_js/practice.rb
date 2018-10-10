4060a596195bde8b6eb493e9a818ac0202d6cf9f
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-10-09 21:25:03 -0500

    10.09.18

---
+++ ruby_v_js/practice.rb
@@ -0,0 +1,28 @@
+# def print_number_100 
+# number = 0
+
+#   while number < 100
+#     number += 1
+#     puts number 
+#   end
+# end
+
+
+
+# def print_everyother
+#   number = 0
+
+#  while number < 100
+#     if number %2 != 0
+#       puts number
+#   end
+# end    
+
+
+# # Equality check
+# x = 10
+# if x == "10"
+#   puts "equal"
+# else
+#   puts "not equal"
+# end
